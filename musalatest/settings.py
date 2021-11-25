a5cc8c134dae0f9dfa38ea3c70ebd564697b1878
Jorge Luis Alfonso <jlalfonso21@gmail.com>
2021-11-25 13:19:10 -0500

    base Django settings for serving

--- musalatest/settings.py
+++ musalatest/settings.py
@@ -115,11 +115,20 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+MEDIA_ROOT = BASE_DIR / "media"
+
+MEDIA_URL = '/media/'
+
+# STATIC_ROOT = BASE_DIR / 'static'
+
 STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
