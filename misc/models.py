8b1fe8b51c8c042b4bca1799c6906aeb1c9176d6
Jorge Luis Alfonso <jlalfonso21@gmail.com>
2021-11-25 11:30:09 -0500

    Drone validations

--- misc/models.py
+++ misc/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 from django.db import models
 
 DRONE_MODELS = (
@@ -18,11 +19,23 @@ DRONE_STATES = (
 
 
 class Drone(models.Model):
-    serial_number = models.CharField(max_length=100)
+    serial_number = models.CharField(max_length=100,
+                                     validators=(
+                                         RegexValidator(
+                                             regex="^[a-zA-Z0-9]([a-zA-Z0-9_-])+$",
+                                             message='Enter a valid value. '
+                                                     'Allowed only letters, underscores, dashes and numbers.'
+                                         ),
+                                     ))
+
     model = models.CharField(max_length=20, choices=DRONE_MODELS, default='middleweight')
-    weight_limit = models.FloatField()
-    battery_capacity = models.FloatField()
-    state = models.CharField(max_length=20, choices=DRONE_STATES)
+
+    weight_limit = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(500), ],
+                                     help_text="Weight in grams (g). 500g max.")
+
+    battery_capacity = models.FloatField(default=100.0, validators=[MinValueValidator(0), MaxValueValidator(100), ])
+
+    state = models.CharField(max_length=20, choices=DRONE_STATES, default='IDLE')
 
     def __str__(self):
         return self.serial_number
