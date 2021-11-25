517dcc969184b910336ed36aba33f6354f9057a9
Jorge Luis Alfonso <jlalfonso21@gmail.com>
2021-11-25 16:22:27 -0500

    get drone battery level

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from api.views.drones import DroneCreate, DroneAvailableList
+from api.views.drones import DroneCreate, DroneAvailableList, DronesViewSet
 from api.views.medications import MedicationListCreate
 
 app_name = 'api'
@@ -8,5 +8,7 @@ app_name = 'api'
 urlpatterns = [
     path('medication/', MedicationListCreate.as_view()),
     path('drones/', DroneCreate.as_view()),
+    path('drones/battery/<str:serial_number>/', DronesViewSet.as_view({'get': 'battery_level'}),
+         name='drone_battery_level'),
     path('drones/available/', DroneAvailableList.as_view()),
 ]
--- api/views/drones.py
+++ api/views/drones.py
@@ -1,4 +1,7 @@
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.response import Response
 
 from api.serializers.drones import DroneBaseSerializer, DroneCreateSerializer
 from misc.models import Drone
@@ -12,3 +15,16 @@ class DroneCreate(CreateAPIView):
 class DroneAvailableList(ListAPIView):
     queryset = Drone.objects.filter(state="IDLE")
     serializer_class = DroneBaseSerializer
+
+
+class DronesViewSet(viewsets.ModelViewSet):
+    queryset = Drone.objects.all()
+    serializer_class = DroneBaseSerializer
+
+    @action(detail=False, methods=['get'])
+    def battery_level(self, request, serial_number=None):
+        obj: Drone = self.queryset.filter(serial_number=serial_number).first()
+        if obj is not None:
+            return Response(data=obj.battery_capacity)
+        else:
+            return Response(data={"detail": "not found"}, status=status.HTTP_404_NOT_FOUND)
