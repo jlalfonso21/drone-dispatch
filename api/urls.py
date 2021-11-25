36a0b4922ebfb10c1d9433377ad948e7b3aefd70
Jorge Luis Alfonso <jlalfonso21@gmail.com>
2021-11-25 16:19:39 -0500

    get available drones

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from api.views.drones import DroneCreate
+from api.views.drones import DroneCreate, DroneAvailableList
 from api.views.medications import MedicationListCreate
 
 app_name = 'api'
@@ -8,4 +8,5 @@ app_name = 'api'
 urlpatterns = [
     path('medication/', MedicationListCreate.as_view()),
     path('drones/', DroneCreate.as_view()),
+    path('drones/available/', DroneAvailableList.as_view()),
 ]
--- api/views/drones.py
+++ api/views/drones.py
@@ -1,9 +1,14 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 
-from api.serializers.drones import DroneCreateSerializer
+from api.serializers.drones import DroneBaseSerializer, DroneCreateSerializer
 from misc.models import Drone
 
 
 class DroneCreate(CreateAPIView):
     queryset = Drone.objects.all()
     serializer_class = DroneCreateSerializer
+
+
+class DroneAvailableList(ListAPIView):
+    queryset = Drone.objects.filter(state="IDLE")
+    serializer_class = DroneBaseSerializer
